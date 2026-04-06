fb58513333b9b3cec78a626bf83e5efc17a0a46e
leonardo7-del <1590329@senati.pe>
2026-04-06 12:40:39 -0500

    Add config mail file as-is

---
+++ config/mail.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'host' => 'smtp.gmail.com',
+    'port' => 587,
+    'encryption' => 'tls',
+    'username' => 'leonardohuarachadelvillar@gmail.com',
+    'password' => 'mlfb zwbe ecrp joss',
+    'from_email' => 'leonardohuarachadelvillar@gmail.com',
+    'from_name' => 'Patron MVC',
+];
